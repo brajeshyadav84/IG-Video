d7daee32bacc33ad21d8bc6bf6fb872487716197
Brajesh <brajesh.mymailbox@gmail.com>
2016-07-02 15:26:15 +0800

    update api

--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,19 @@
-angular.module('starter.services', [])
-
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+IG.service('IGService',function($http, $q, $ionicLoading, $timeout) {
+    return {
+        postAPI: function (request, url) {
+                var deferred = $q.defer();
+                $ionicLoading.show({template: 'Loading...'});
+                console.log(url);console.log(request);
+                return $http.get(url)
+                    .success(function (response) {
+                        $ionicLoading.hide();
+                        deferred.resolve(response);
+                    })
+                    .error(function (error) {
+                        $ionicLoading.hide();
+                        deferred.reject(error);
+                    });
+                return deferred.promise;
         }
-      }
-      return null;
     }
-  };
 });
