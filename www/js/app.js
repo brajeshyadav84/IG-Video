06e9f8ba75562adfbd13baed2f9cfa68a6a3b806
Brajesh <brajesh.mymailbox@gmail.com>
2016-08-13 11:37:22 +0800

    fixed for showing youtube videos

--- www/js/app.js
+++ www/js/app.js
@@ -12,13 +12,13 @@ IG.run(function($ionicPlatform, $rootScope) {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      //cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      //cordova.plugins.Keyboard.disableScroll(true);
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
 
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
-      //StatusBar.styleDefault();
+      StatusBar.styleDefault();
     }
       
   });
