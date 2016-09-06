7fa824bb2a796016a066c19164bdaf35a1ad459c
Brajesh <brajesh.mymailbox@gmail.com>
2016-09-06 18:50:51 +0800

    Tweaks

--- www/js/homeController.js
+++ www/js/homeController.js
@@ -39,7 +39,7 @@ IG.controller('homeController', function($scope, $http, $rootScope, $state, $sta
     
     $scope.onClickShare = function (){
         $scope.reset ();
-        $cordovaSocialSharing.share('Hi!, Its nice app for IT professional (Developers)', 'InterviewGully Portal', null, 'http://www.interviewgully.com/app');
+        $cordovaSocialSharing.share('Hi!, It is nice app for frontend Developers', 'InterviewGully', null, 'http://www.interviewgully.com/appLauncher.html');
     };
     
     $scope.onClickContact = function (){
