b2d440795a8b68d1591f4eac30179e5c29b2545b
Brajesh <brajesh.mymailbox@gmail.com>
2016-10-08 18:48:52 +0800

    Force Upgrade Implementation

--- www/js/homeController.js
+++ www/js/homeController.js
@@ -14,6 +14,33 @@ IG.controller('homeController', function($scope, $http, $rootScope, $state, $sta
     },function(error){
         console.log(error);
     });
+
+    // start force upgrade Implememntation
+
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    var isIOS = (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/iPod/i));
+    var isAndroid = (userAgent.match(/Android/i));
+    var isDesktop = !(isIOS || isAndroid);
+
+    var forceurl = "http://www.interviewgully.com/API/IG_Video/IGUpgrade.json";
+    IGService.postAPI(request, forceurl).then( function(response){
+        var data = response.data;
+        if(!!isAndroid){
+            var id = data.androidSettings.versions[0];
+            if(id != "2.0"){
+                alert('Loading android upgrade screen');
+            }
+        } else {
+            var id = data.iosSettings.versions[0];
+            if(id != "2.0"){
+                console.log('Loading ios upgrade screen');
+            }
+        }
+        
+    },function(error){
+        console.log(error);
+    });
+    // end force upgrade Implememntation
     
     $scope.reset = function () {
         $scope.isShow = {
