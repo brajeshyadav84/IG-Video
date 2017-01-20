73ac8342a213f6120cafc2ca25cae4828de84f51
Brajesh <brajesh.mymailbox@gmail.com>
2017-01-20 17:47:58 +0800

    email functionality Implementation.

--- www/js/playController.js
+++ www/js/playController.js
@@ -7,5 +7,25 @@ IG.controller('playController', function($sce, $scope, $rootScope, $state, $stat
     
     $scope.videoID = ID;
     
-    
+    $scope.btnSendQuery= function() {
+        if(window.plugins && window.plugins.emailComposer) {
+            window.plugins.emailComposer.showEmailComposerWithCallback(function(result) {
+                console.log("Response -> " + result);
+            }, 
+            "Question from IG App", // Subject
+            $scope.Question,                      // Body
+            ["brajesh.mymailbox@gmail.com"],    // To
+            $scope.EmailID,                    // CC
+            null,                    // BCC
+            false,                   // isHTML
+            null,                    // Attachments
+            null);                   // Attachment Data
+        }
+    }
+
+    $scope.btnCancel= function() {
+    	$scope.EmailID = "";
+    	$scope.Question = "";
+    }
+
 });
