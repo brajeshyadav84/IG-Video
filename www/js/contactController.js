9dcb7b1590a8555386b8f5bddcff43ee42cced7f
Brajesh <brajesh.mymailbox@gmail.com>
2017-01-22 16:28:45 +0800

    Email Functionality

--- www/js/contactController.js
+++ www/js/contactController.js
@@ -2,5 +2,21 @@ IG.controller('contactController', function($scope, $rootScope, $state, $statePa
     console.log("contact");
     $rootScope.title = "Contact Us";
     $rootScope.back = "backShow";
+
+    $scope.btnQuery= function(Subject, emailID) {
+        if(window.plugins && window.plugins.emailComposer) {
+            window.plugins.emailComposer.showEmailComposerWithCallback(function(result) {
+                console.log("Response -> " + result);
+            }, 
+            Subject, // Subject
+            null,                      // Body
+            [emailID],    // To
+            null,                    // CC
+            null,                    // BCC
+            false,                   // isHTML
+            null,                    // Attachments
+            null);                   // Attachment Data
+        }
+    }
     
 });
